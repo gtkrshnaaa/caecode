0641f2bc0e0b03d8bee8b593bf2daa63e504c272
gtkrshnaaa <gtkrshnaaa@gmail.com>
2026-02-21 17:05:30 +0700

    chore: bump version to 0.1.1 and add -v flag

--- core/includes/app_state.h
+++ core/includes/app_state.h
@@ -6,7 +6,7 @@
 #include <vte/vte.h>
 
 // Version and Constants
-#define VERSION "0.1.0"
+#define VERSION "0.1.1"
 
 // Global UI Widgets (externed for module access)
 extern GtkWidget *window;
--- core/src/main.c
+++ core/src/main.c
@@ -1,6 +1,15 @@
 #include "ui.h"
 
+#include <stdio.h>
+#include <string.h>
+
 int main(int argc, char **argv) {
+    if (argc > 1) {
+        if (strcmp(argv[1], "-v") == 0 || strcmp(argv[1], "--version") == 0) {
+            printf("Caecode version %s\n", VERSION);
+            return 0;
+        }
+    }
     gtk_init(&argc, &argv);
     
     create_main_window();
