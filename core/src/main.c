2fa0b73cb42f5e857100cc8ebc686b13609cbad3
gtkrshnaaa <gtkrshnaaa@gmail.com>
2026-02-21 18:35:03 +0700

    feat: enable CLI directory arguments (e.g. caecode .)

--- core/src/main.c
+++ core/src/main.c
@@ -2,17 +2,49 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include "sidebar.h"
+
+static gboolean invoke_initial_folder_open(gpointer data) {
+    char *path = (char *)data;
+    if (path) {
+        open_folder(path);
+        g_free(path);
+    }
+    return G_SOURCE_REMOVE;
+}
 
 int main(int argc, char **argv) {
+    char *target_dir = NULL;
+    
     if (argc > 1) {
         if (strcmp(argv[1], "-v") == 0 || strcmp(argv[1], "--version") == 0) {
             printf("Caecode version %s\n", VERSION);
             return 0;
+        } else if (argv[1][0] != '-') {
+            // Treat as a directory path
+            char abs_path[PATH_MAX];
+            if (realpath(argv[1], abs_path) != NULL) {
+                struct stat path_stat;
+                if (stat(abs_path, &path_stat) == 0 && S_ISDIR(path_stat.st_mode)) {
+                    target_dir = g_strdup(abs_path);
+                } else {
+                    fprintf(stderr, "Error: '%s' is not a valid directory\n", argv[1]);
+                }
+            } else {
+                fprintf(stderr, "Error: Cannot resolve path '%s'\n", argv[1]);
+            }
         }
     }
+    
     gtk_init(&argc, &argv);
     
     create_main_window();
+    
+    if (target_dir) {
+        g_idle_add(invoke_initial_folder_open, target_dir);
+    }
     
     gtk_main();
 
