e2345c8247e3ab5afbd67c2def4c96a1f41d05dc
gtkrshnaaa <gtkrshnaaa@gmail.com>
2026-02-21 18:45:38 +0700

    fix: call close_folder on exit for complete memory cleanup

--- core/src/main.c
+++ core/src/main.c
@@ -49,6 +49,7 @@ int main(int argc, char **argv) {
     gtk_main();
 
     // Cleanup
+    close_folder();
     if (last_saved_content) g_free(last_saved_content);
     if (current_file_row_ref) gtk_tree_row_reference_free(current_file_row_ref);
 
